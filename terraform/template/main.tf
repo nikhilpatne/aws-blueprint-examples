b3d6a16d47b41f2edabb780ad1b429f65c48f46c
nikhilpatne <66066035+nikhilpatne@users.noreply.github.com>
2021-03-25 09:05:17 +0530

    Update main.tf

--- terraform/template/main.tf
+++ terraform/template/main.tf
@@ -9,6 +9,9 @@ provider "aws" {
 resource "aws_vpc" "example_vpc" {
   cidr_block = "10.10.0.0/16"
   enable_dns_hostnames = true
+  tags = {
+    Name = "Nikhil"
+  }
 }
 
 resource "aws_internet_gateway" "example_gateway" {
