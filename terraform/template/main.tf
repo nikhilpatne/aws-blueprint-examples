f14e91c1c7164a0a3c595f38473e1a0090b2471e
nikhilpatne <66066035+nikhilpatne@users.noreply.github.com>
2021-03-25 14:11:54 +0530

    Update main.tf

--- terraform/template/main.tf
+++ terraform/template/main.tf
@@ -1,15 +1,9 @@
-# Specify the provider and access details
-provider "aws" {
-  region = var.aws_region
-  access_key = var.access_key
-  secret_key = var.secret_key
+provider "azurerm" {
+  features {}
 }
 
-# Create a VPC
-resource "aws_vpc" "example_vpc" {
-  cidr_block = "10.10.0.0/16"
-  enable_dns_hostnames = true
-  tags = {
-    Name = "Nikhil-999"
-  }
+# Create a resource group
+resource "azurerm_resource_group" "example_rg" {
+  name     = var.resource_group_name
+  location = "eastus"
 }
