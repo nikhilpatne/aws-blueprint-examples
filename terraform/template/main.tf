4683c86dd5bf8cbc5bae2f75020fe2448a8ad764
nikhilpatne <66066035+nikhilpatne@users.noreply.github.com>
2021-03-25 13:11:38 +0530

    Update main.tf

--- terraform/template/main.tf
+++ terraform/template/main.tf
@@ -10,22 +10,6 @@ resource "aws_vpc" "example_vpc" {
   cidr_block = "10.10.0.0/16"
   enable_dns_hostnames = true
   tags = {
-    Name = "Nikhil"
+    Name = "Nikhil-999"
   }
-}
-
-resource "aws_internet_gateway" "example_gateway" {
-  vpc_id = aws_vpc.example_vpc.id
-}
-
-# Create a subnet to launch our instances into
-resource "aws_subnet" "example_subnet" {
-  vpc_id                  = aws_vpc.example_vpc.id
-  cidr_block              = "10.10.4.0/24"
-  map_public_ip_on_launch = false
-  availability_zone = var.aws_zone
-}
-
-output "aws_region" {
-  value = var.aws_region
 }
