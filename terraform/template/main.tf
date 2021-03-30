8c4b9a172104030ba1bcf9154bb9d216f73a4ede
nikhilpatne <66066035+nikhilpatne@users.noreply.github.com>
2021-03-30 09:25:50 +0530

    Update main.tf

--- terraform/template/main.tf
+++ terraform/template/main.tf
@@ -1,23 +1,43 @@
 
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.46.0"
-    }
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "=2.46.0"
+#     }
+#   }
+# }
+
+# # Configure the Microsoft Azure Provider
+# provider "azurerm" {
+#   features {}
+
+# }
+
+
+
+# # Create a resource group
+# resource "azurerm_resource_group" "example_rg" {
+#   name     = "asd
+#   location = "eastus"
+# }
+
+
+
+
+# Specify the provider and access details
+provider "aws" {
+  region = var.aws_region
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+# Create a VPC
+resource "aws_vpc" "example_vpc" {
+  cidr_block = "10.10.0.0/16"
+  enable_dns_hostnames = true
+  tags = {
+    "name" = "test"
   }
 }
 
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  features {}
-
-}
-
-
-
-# Create a resource group
-resource "azurerm_resource_group" "example_rg" {
-  name     = "asd
-  location = "eastus"
-}
